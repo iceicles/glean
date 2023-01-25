42f9e332a286065b8180c3fa5408993d7d843944
oshiogifted <oshiogifted2011@gmail.com>
2023-01-25 18:38:58 -0500

    defining pages and routes

---
+++ src/pages/Diary.js
@@ -0,0 +1,13 @@
+function DiaryPage() {
+  return (
+    <>
+      <h1>Recents</h1>
+      <p>Diary Page</p>
+      <textarea></textarea>
+
+      <h1>Previous...</h1>
+    </>
+  );
+}
+
+export default DiaryPage;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <h2>Welcome to Glean!</h2>
+      <p>Log in or Sign up to get started</p>
+    </>
+  );
+}
+
+export default HomePage;
