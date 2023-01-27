87ca6b834b3d325ec14ece031e68f102278c5c7c
oshiogifted <oshiogifted2011@gmail.com>
2023-01-27 14:26:34 -0500

    [feat] implemented two-way binding between editor and canvas

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,28 +1,46 @@
 import html2canvas from 'html2canvas';
-import { useEffect, useState } from 'react';
-import EditDiary from '../components/Editor';
-import Modal from '../components/Modal';
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function DiaryPage() {
   const [editMode, setEditMode] = useState(false);
+  const [editValue, setEditValue] = useState('');
 
   /**
    * This function should only run when the user creates a new diary, and clicks save.
    * We only want to take a screenshot once.
    */
   function takeScreenshot() {
-    var textarea = document.querySelector('.rdw-editor-main');
+    var editor = document.querySelector('.ql-editor');
 
-    html2canvas(textarea, {
+    html2canvas(editor, {
       width: 300,
       height: 200,
     }).then(function (canvas) {
+      if (!editValue) return;
       document.body.appendChild(canvas);
+      setEditMode(false);
+
+      console.log('value outside click - ', editValue);
+
+      canvas.addEventListener('click', function () {
+        console.log('canvas clicked');
+        console.log('value - ', editValue);
+        setEditMode(true);
+        // how to get the value from the canvas and use that to fill the text editor??
+        setEditValue(editValue);
+      });
     });
   }
 
   function toggleEditMode() {
     return setEditMode(!editMode);
+  }
+
+  function createNewDiary() {
+    setEditMode(true);
+    setEditValue('');
   }
 
   return (
@@ -32,7 +50,10 @@ function DiaryPage() {
       <h1>Previous...</h1>
       <button onClick={toggleEditMode}>Toggle Edit</button>
       <button onClick={takeScreenshot}>Save</button>
-      {editMode && <EditDiary />}
+      <button onClick={createNewDiary}>Create New Diary</button>
+      {editMode && (
+        <ReactQuill theme='snow' value={editValue} onChange={setEditValue} />
+      )}
     </>
   );
 }
