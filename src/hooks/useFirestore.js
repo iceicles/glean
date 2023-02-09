800b8590badf785f02d06ad15011f02605d8b285
oshiogifted <oshiogifted2011@gmail.com>
2023-02-09 18:24:24 -0500

    fixed card re-rendering when editor data changes

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,14 +1,15 @@
 import { useState, useEffect } from 'react';
 import { projectFirestore } from '../firebase/config';
-import { doc, setDoc } from 'firebase/firestore';
-import { onSnapshot, collection } from 'firebase/firestore';
+import { doc, getDocs, setDoc } from 'firebase/firestore';
+import { collection } from 'firebase/firestore';
 
 const useFirestore = (
   topCollection,
   userName,
   entriesCollection,
   entries,
-  entry
+  entry,
+  newData = false
 ) => {
   const [data, setData] = useState([]);
   useEffect(() => {
@@ -19,9 +20,10 @@ const useFirestore = (
     );
 
     // creates a new document with entry as its data
-    setDoc(entryDocumentRef, {
-      entry,
-    });
+    newData &&
+      setDoc(entryDocumentRef, {
+        entry,
+      });
 
     // gets the 'Entries' collection reference
     const entriesCollectionRef = collection(
@@ -30,19 +32,21 @@ const useFirestore = (
     );
 
     // listens to and returns changes in firestore
-    const unsubscribe = onSnapshot(entriesCollectionRef, (docSnap) => {
+    const unsub = async () => {
+      // using getDocs instead of onSnapshot so that the card components don't always re-render when
+      // data changes
+      const docSnap = await getDocs(entriesCollectionRef);
       // loop through the documents in Entries collection and store in an array
-
       const documents = [];
       docSnap.forEach((doc) => {
         // doc is each documents in Entries collection (ie., Entry-0, Entry-1, etc)
         documents.push({ ...doc.data(), id: doc.id });
       });
       setData(documents);
-    });
+    };
 
-    return () => unsubscribe();
-  }, [topCollection, userName, entriesCollection, entries, entry]);
+    return () => unsub();
+  }, [topCollection, userName, entriesCollection, entries, entry, newData]);
 
   return { data };
 };
