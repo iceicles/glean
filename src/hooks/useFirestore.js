812a36f9ffad405733a0869228ae52eacbb0d323
oshiogifted <oshiogifted2011@gmail.com>
2023-02-20 23:09:52 -0500

    ordering document entries in desc by timeStamp

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,7 +1,14 @@
 import { useState, useEffect } from 'react';
 import { projectFirestore } from '../firebase/config';
-import { doc, getDocs, setDoc } from 'firebase/firestore';
-import { collection } from 'firebase/firestore';
+import {
+  doc,
+  collection,
+  getDocs,
+  setDoc,
+  query,
+  orderBy,
+  serverTimestamp,
+} from 'firebase/firestore';
 
 const useFirestore = (
   topCollection,
@@ -23,6 +30,7 @@ const useFirestore = (
     newData &&
       setDoc(entryDocumentRef, {
         entry,
+        timeStamp: serverTimestamp(),
       });
 
     // gets the 'Entries' collection reference
@@ -31,18 +39,24 @@ const useFirestore = (
       `${topCollection}/${userName}/${entriesCollection}`
     );
 
+    // orders the document in 'Entries' collection by timeStamp in desc order
+    const colQuery = query(entriesCollectionRef, orderBy('timeStamp', 'desc'));
+
     // listens to and returns changes in firestore
     const unsub = async () => {
-      // using getDocs instead of onSnapshot so that the card components don't always re-render when
-      // data changes
-      const docSnap = await getDocs(entriesCollectionRef);
+      // using getDocs instead of onSnapshot so that the card components don't always re-render when data changes
+      const docSnap = await getDocs(colQuery);
       // loop through the documents in Entries collection and store in an array
       const documents = [];
       docSnap.forEach((doc) => {
         // doc is each documents in Entries collection (ie., Entry-0, Entry-1, etc)
-        documents.push({ ...doc.data(), id: doc.id });
+        documents.push({
+          ...doc.data(),
+          id: doc.id,
+        });
       });
       setData(documents);
+      console.log('data - ', data);
     };
 
     return () => unsub();
